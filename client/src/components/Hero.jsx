3d2884250305f4b7ebd6e761bcace29dac185c60
Luis Fernando Vargas <blackfer2003@gmail.com>
2024-09-05 19:28:33 -0500

    Arreglos en la landing

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,25 +5,26 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
 
 const Hero = () => {
     const parallaxRef = useRef(null);
 
   return (
-    <Section className="pt-[19rem] -mt-[8rem]" 
+    <Section className="pt-[12rem] -mt-[5.25rem]" 
     crosses 
-    crossesOffset="lg:translate-y-[8rem]"
+    crossesOffset="lg:translate-y-[5.25em]"
     CustomPaddings
     id="hero"
     
     >
         <div className="container relative" ref={parallaxRef}>
-            <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 
-            lg:mb:[6rem]">
+            <div className="relative z-1 max-w-[85rem] mx-auto text-center mb-[3.875rem] md:mb-20 
+            lg:mb-[6rem]">
                 <h1 className="h1 mb-6">
-                    Explora la IA Chateando con
+                    Explora las posibilidades de IA Chateando con {` `}
                     <span className="inline-block relative">
-                        TimeBridge 
+                        TimeBridge {" "}
                         <img 
                             src={curve} 
                             className="pt-3 absolute top-full left-0 w-full xl:-mt-2"
@@ -33,17 +34,17 @@ const Hero = () => {
                         />
                     </span>
                 </h1>
-                <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8 pt-2">Desata el poder de la IA con TimeBridge.
+                <p className="body-1 max-w-5xl mx-auto mb-6 text-n-2 lg:mb-8 pt-2">Desata el poder de la IA con TimeBridge.
                     Mejora tu productivdad Con TimeBridge, la aplicacion abierta para IA 
                 </p>
                 <Button href="/pricing" white>
                     Comienza
                 </Button>
             </div>
-            <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+            <div className="relative max-w-[23rem] mx-auto md:max-w-7xl xl:mb-24">
                 <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
                     <div className="relative bg-n-8 rounded-[1rem]">
-                        <div className="h-[1.1rem] bg-n-10 rounded-t-[0.9rem]"/>
+                        <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]"/>
                         <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] 
                         lg:aspect-[1024/490]">
                             <img 
@@ -53,6 +54,11 @@ const Hero = () => {
                             width={1024}
                             height={490}
                             alt="IA" />
+
+                            <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto 
+                            md:bottom-8 md:w-[31rem] md:-translate-x-1/2"/>
+
+
                             <ScrollParallax isAbsolutelyPositioned>
                                 <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 
                                 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex ">
@@ -66,12 +72,12 @@ const Hero = () => {
 
                                         </li>
                                     ))}
-
                                 </ul>
-
                             </ScrollParallax>
                         </div>
                     </div>
+                    <Gradient/>
+
                 </div>
                 <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top[46%] 
                 md:w-[138%] lg:-top-[104%]">
@@ -83,8 +89,11 @@ const Hero = () => {
                     alt="hero"
                     />
                 </div>
+
+                <BackgroundCircles/>
             </div>
         </div>
+        <BottomLine/>
     </Section>
   )
 }
--- client/src/components/design/Hero.jsx
+++ client/src/components/design/Hero.jsx
@@ -15,7 +15,7 @@ export const Gradient = () => {
 export const BottomLine = () => {
   return (
     <>
-      <div className="hidden absolute top-[55.25rem] left-10 right-10 h-0.25 bg-n-6 pointer-events-none xl:block" />
+      <div className="hidden absolute top-[55.25rem] left-10 right-10 h-0.25 bg-n-9 pointer-events-none xl:block" />
 
       <PlusSvg className="hidden absolute top-[54.9375rem] left-[2.1875rem] z-2 pointer-events-none xl:block" />
 
@@ -27,10 +27,10 @@ export const BottomLine = () => {
 const Rings = () => {
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 w-[65.875rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-1/2 left-1/2 w-[51.375rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-1/2 left-1/2 w-[36.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-1/2 left-1/2 w-[23.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
+      <div className="absolute top-1/2 left-1/2 w-[68.875rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
+      <div className="absolute top-1/2 left-1/2 w-[53.375rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
+      <div className="absolute top-1/2 left-1/2 w-[38.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
+      <div className="absolute top-1/2 left-1/2 w-[25.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
     </>
   );
 };
@@ -43,7 +43,7 @@ export const BackgroundCircles = ({ parallaxRef }) => {
   }, []);
 
   return (
-    <div className="absolute -top-[42.375rem] left-1/2 w-[78rem] aspect-square border border-n-2/5 rounded-full -translate-x-1/2 md:-top-[38.5rem] xl:-top-[32rem]">
+    <div className="absolute -top-[42.375rem] left-1/2 w-[83rem] aspect-square border border-n-2/5 rounded-full -translate-x-1/2 md:-top-[38.5rem] xl:-top-[32rem]">
       <Rings />
 
       {/* Moving background colored circle balls */}
